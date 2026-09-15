5c858a7c6040b9375b2fbfb1586a4b7d008f6e60
pro grammer <milesplet847@gmail.com>
2026-09-15 10:45:07 +0200

    sorteren

--- Oefenopdrachten/Opdracht 1/sketch.js
+++ Oefenopdrachten/Opdracht 1/sketch.js
@@ -1,23 +1,36 @@
 function setup() {
-  createCanvas(800
-, 800);
-  console.log("Hello World")
+  createCanvas(600, 400);
 }
 
 function draw() {
-  background(220);
-  fill(100);
-  rect(300,300,100,300);
-  fill(0,255,0);
-  circle(350,550,80);
-  fill("orange");
-  circle(350,450,80);
-  fill("red");
-  circle(350,350,80);
-  fill(100);
-  rect (325,600,50,400);
-  noStroke(1)
-  
+  background(255);
+  strokeWeight(6)
+  fill("blue")
+  rect(0,0,100,35);
+  fill("red")
+  rect(0,35,100,200);
+  fill("yellow")
+  rect(100,0,200,140)
+   fill("white")
+  rect(300,0,200,140)
+  fill("white")
+  rect(0,140,150,300)
+  fill("red")
+  rect(40,140,150,300)
+  fill("black")
+  rect(0,230,200,400)
+  fill("red")
+  rect(0,230,440,123)
+   fill("white")
+  rect(300,195,143,200);
+  fill("blue")
+  rect(154,140,143,90)
+   fill("black")
+  rect(0,230,200,)
+  fill("red")
+  rect(300,140,143,55)
+   fill("blue")
+  rect(445,195,143,200);
+  fill("black")
+  rect(445,140,143,50)
 }
-
-//comment
--- Oefenopdrachten/Opdracht 2/sketch.js
+++ Oefenopdrachten/Opdracht 2/sketch.js
@@ -1,36 +1,28 @@
+let x = 100
+let y = 80
+let greeting = "Hello World!"
+let a = 20
+let b = 10
+let optellen = a + b
+let aftrekken = a - b
+let vermenigvuldigen = a * b
+let delen = a / b
+
 function setup() {
-  createCanvas(600, 400);
+  createCanvas(400, 400);
 }
 
 function draw() {
-  background(255);
-  strokeWeight(6)
-  fill("blue")
-  rect(0,0,100,35);
-  fill("red")
-  rect(0,35,100,200);
-  fill("yellow")
-  rect(100,0,200,140)
-   fill("white")
-  rect(300,0,200,140)
-  fill("white")
-  rect(0,140,150,300)
-  fill("red")
-  rect(40,140,150,300)
-  fill("black")
-  rect(0,230,200,400)
-  fill("red")
-  rect(0,230,440,123)
-   fill("white")
-  rect(300,195,143,200);
-  fill("blue")
-  rect(154,140,143,90)
-   fill("black")
-  rect(0,230,200,)
-  fill("red")
-  rect(300,140,143,55)
-   fill("blue")
-  rect(445,195,143,200);
-  fill("black")
-  rect(445,140,143,50)
+  background(220);
+
+  text(x, 20, 20);
+  text(greeting, 20, 60);
+
+  text("Optellen = " + optellen, 20, y);
+  noLoop(y = y+ 20);
+  text("Aftrekken = " + aftrekken, 20, y);
+  noLoop(y = y + 20);
+  text("Vermenigvuldigen = " + vermenigvuldigen, 20, y);
+  noLoop(y = y + 20);
+  text("Delen = " + delen, 20, y);
 }
