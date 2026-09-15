77163fade5a21cfc66e4c0d874c97a09da3a95e8
pro grammer <milesplet847@gmail.com>
2026-09-15 13:48:59 +0200

    wolken

--- Week 2/sketch.js
+++ Week 2/sketch.js
@@ -6,19 +6,41 @@ let skycolor
 let suncolor
 let isDay = true;
 let stars = []
-let cloudGroups = []   
 
 circleX = 100
 circleY = 115
 let circlediameter = 100
+// Wolk 1
+let wolkX1 = 300;
+let wolkY1 = 85;
+let wolkDiameter1 = 70;
+
+let wolkX2 = 330;
+let wolkY2 = 80;
+let wolkDiameter2 = 70;
+
+let wolkX3 = 360;
+let wolkY3 = 85;
+let wolkDiameter3 = 70;
+
+// Wolk 2
+let wolkX4 = 500;
+let wolkY4 = 155;
+let wolkDiameter4 = 70;
+
+let wolkX5 = 530;
+let wolkY5 = 150;
+let wolkDiameter5 = 70;
+
+let wolkX6 = 560;
+let wolkY6 = 155;
+let wolkDiameter6 = 70;
 function setup() {
   canvasy =(715)
   canvasx =(1525)
   createCanvas(canvasx, canvasy);
 
-  for (let i = 0; i < 3; i++) {
-  cloudGroups.push(new CloudGroup(random(canvasx), random(50, 200)));
-}
+  
   for (let i = 0; i < 150; i++) {
     stars.push({ x: random(canvasx), y: random(525), size: random(1, 4) })
   }
@@ -74,11 +96,27 @@ if (circleX > canvasx) {
   triangle(500, 775, 700, 350, 900, 775)
   triangle(300, 775, 500, 450, 700, 775)
 
-//wolken
-for (let g of cloudGroups) {
-  g.update();
-  g.show();
-}   
+
+// wolken
+strokeWeight(0)
+  fill("#ffffff")
+  circle(wolkX1, wolkY1, wolkDiameter1);
+  wolkX1 = wolkX1 + 0.2;
+
+  circle(wolkX2, wolkY2, wolkDiameter2);
+  wolkX2 = wolkX2 + 0.2;
+
+  circle(wolkX3, wolkY3, wolkDiameter3);
+  wolkX3 = wolkX3 + 0.2;
+
+  circle(wolkX4, wolkY4, wolkDiameter4);
+  wolkX4 = wolkX4 + 0.1;
+ 
+  circle(wolkX5, wolkY5, wolkDiameter5);
+  wolkX5 = wolkX5 + 0.1;
+
+  circle(wolkX6, wolkY6, wolkDiameter6);
+  wolkX6 = wolkX6 + 0.1;
 
   //weg
   strokeWeight(1)
@@ -108,56 +146,3 @@ rect(1275,600,150,25,20)
   rect(937.5, 600, 25, 200);
   
 }
-class CloudGroup {
-  constructor(x, y) {
-    this.pos = createVector(x, y);
-    this.vel = createVector(random(0.3, 0.7), 0);
-    this.clouds = [];
-    let n = floor(random(4, 8));
-    for (let i = 0; i < n; i++) {
-      this.clouds.push({
-        ox: random(-60, 60),
-        oy: random(-20, 20),
-        puffs: this.makePuffs()
-      });
-    }
-  }
-
-  makePuffs() {
-    let puffs = [];
-    let n = floor(random(4, 8));
-    for (let i = 0; i < n; i++) {
-      puffs.push({
-        ox: random(-20, 20),
-        oy: random(-10, 10),
-        r: random(18, 35)
-      });
-    }
-    return puffs;
-  }
-
-  update() {
-    this.pos.add(this.vel);
-    if (this.pos.x > canvasx + 120) {
-      this.pos.x = -120;
-      for (let c of this.clouds) {
-        c.ox = random(-60, 60);
-        c.oy = random(-20, 20);
-        c.puffs = this.makePuffs();
-      }
-    }
-  }
-
-  show() {
-    for (let c of this.clouds) {
-      push();
-      translate(this.pos.x + c.ox, this.pos.y + c.oy);
-      noStroke();
-      fill(255, 220);
-      for (let p of c.puffs) {
-        circle(p.ox, p.oy, p.r);
-      }
-      pop();
-    }
-  }
-}   
