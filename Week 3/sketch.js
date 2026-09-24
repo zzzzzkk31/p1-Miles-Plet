a58967741623ec24da0eea63d6c00af57e34a3fe
pro grammer <milesplet847@gmail.com>
2026-09-24 11:50:02 +0200

    efdsdsfdf

--- Week 3/sketch.js
+++ Week 3/sketch.js
@@ -52,6 +52,12 @@ let bluesTurn = ("Blue's turn");
 // Geluiden
 let klikGeluid;
 
+let img;
+
+function preload() {
+  img = loadImage("Boterkaaseneieren.png-1600x900.png");
+}
+
 function setup() {
   createCanvas(400, 400);
 
@@ -64,21 +70,13 @@ function speelKlik() {
   if (klikGeluid && klikGeluid.isLoaded()) {
     klikGeluid.play();
   }
-}
-
-
+} 
 
 function draw() {
   background(220);
 
-  // Achtergrond van het veld
-  strokeWeight(0);
-  fill("blue");
-  rect(0, 0, 200, 400);
-  fill("red");
-  rect(195, 0, 210, 400);
-  fill("#000000")
-  rect(40,40,300,300,10)
+  // Achtergrond
+  image(img, 0, 0, 400, 400);
 
   // Vakjes tekenen
   strokeWeight(5);
